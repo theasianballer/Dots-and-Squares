d835f1276828a84e37f826c8bbf15fa5a3f6f691
Sampreeth Krovvidi <220362@s020rmc148-05.local>
2018-12-07 12:01:41 -0600

    Testing

--- Dots and Squares/Dots and Squares/ViewController.swift
+++ Dots and Squares/Dots and Squares/ViewController.swift
@@ -17,9 +17,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    var name : String = "Sam"
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
+
 
