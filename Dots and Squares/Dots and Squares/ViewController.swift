114c3f1506ba8e1e58e97cdf4cf9d73f4d455e71
theasianballer <38295885+theasianballer@users.noreply.github.com>
2018-12-07 11:41:42 -0600

    Add files via upload

---
+++ Dots and Squares/Dots and Squares/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dots and Squares
+//
+//  Created by Currie, Chance K on 12/7/18.
+//  Copyright © 2018 Currie, Chance K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // The main thing
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
