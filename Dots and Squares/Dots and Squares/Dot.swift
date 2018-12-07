d1379ef7cafc6afc4faa7e0427ba4633bc548ff4
Bellefeuille, Neo M <157000@s020rmc148-01.local>
2018-12-07 12:12:47 -0600

    DotclassSetup

---
+++ Dots and Squares/Dots and Squares/Dot.swift
@@ -0,0 +1,24 @@
+//
+//  Dot.swift
+//  Dots and Squares
+//
+//  Created by Bellefeuille, Neo M on 12/7/18.
+//  Copyright © 2018 Currie, Chance K. All rights reserved.
+//
+
+import Foundation
+
+class Dot
+{
+    var xpos = 0
+    var ypos = 0
+    
+    func setxpos(setTo: Int)
+    {
+        xpos = setTo
+    }
+    func setypos(setTo: Int)
+    {
+        ypos = setTo
+    }
+}
