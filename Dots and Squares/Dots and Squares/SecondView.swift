760dcc35b9bdd7ef627cfbc4cc8298194d75b069
Jolley, Dallin P <201613@s020rmc148-04.local>
2018-12-11 12:12:37 -0600

    created buttons for Setting Menu

---
+++ Dots and Squares/Dots and Squares/SecondView.swift
@@ -0,0 +1,9 @@
+//
+//  SecondView.swift
+//  Dots and Squares
+//
+//  Created by Jolley, Dallin P on 12/11/18.
+//  Copyright © 2018 Currie, Chance K. All rights reserved.
+//
+
+import Foundation
