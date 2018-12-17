5408edf9fee66e4fe3993133a80819d01f3c99d8
Bellefeuille, Neo M <157000@s020rmc148-01.local>
2018-12-17 11:59:48 -0600

    commit of player class(not sure if used or not)

---
+++ Dots and Squares/Dots and Squares/Player.swift
@@ -0,0 +1,28 @@
+//
+//  Player.swift
+//  Dots and Squares
+//
+//  Created by Bellefeuille, Neo M on 12/17/18.
+//  Copyright © 2018 Currie, Chance K. All rights reserved.
+//
+import Foundation
+import UIKit
+
+struct Player
+{
+    var intials: String
+    var color: UIColor
+    func getIntials() -> String
+    {
+        return intials
+    }
+    func getPlayerColor() -> UIColor
+    {
+        return color
+    }
+    mutating func setIntials(to intial: String)
+    {
+        intials = intial
+    }
+}
+
