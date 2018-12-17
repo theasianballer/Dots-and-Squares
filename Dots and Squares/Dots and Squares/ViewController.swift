029b75421047307daf823b45c5ac8d6492e6b87b
Bellefeuille, Neo M <157000@s020rmc148-01.local>
2018-12-17 11:10:27 -0600

    merge

--- Dots and Squares/Dots and Squares/ViewController.swift
+++ Dots and Squares/Dots and Squares/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var dotArray: [[Dot]] = []
+    var array: [[Line]] = []
     
     // The main thing
     // secondary thing
@@ -18,6 +19,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        generateLineArray(5, 6);
         generateDotArray(5, 6);
     }
 
@@ -26,7 +28,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    func generateLineArray(_ x: Int, _ y: Int) {
+        for i in 0...(2*y) {
+            array.append([Line]())
+            for j in 0...(x - (i+1)%2) {
+                array[i].append(Line(xIndex: j, yIndex: i))
+            }
+        }
+        for row in array {
+            for line in row {
+                let button = UIButton(frame: CGRect(x: line.xPos+125, y: line.yPos+125, width: line.orientation == .HORIZONTAL ? 50 : 10, height: line.orientation == .VERTICAL ? 50 : 10))
+                button.backgroundColor = UIColor.lightGray
+                button.setTitle("", for: [])
+                
+                button.addTarget(self, action: #selector(linePressed), for: .touchUpInside)
+                
+                self.view.addSubview(button)
+            }
+        }
+                
+        print(array)
+    }
+        
     func generateDotArray(_ x: Int, _ y: Int) {
         for i in 0...(y) {
             dotArray.append([Dot]())
@@ -36,18 +59,23 @@ class ViewController: UIViewController {
         }
         for row in dotArray {
             for dot in row {
-                let button = UIButton(frame: CGRect(x: dot.xpos, y: dot.ypos, width: 10, height: 10))
+                let button = UIButton(frame: CGRect(x: dot.xpos+125, y: dot.ypos+125, width: 10, height: 10))
                 button.backgroundColor = UIColor.black
                 button.setTitle("", for: [])
                 button.addTarget(self, action: #selector(dotPressed), for: .touchUpInside)
+                
                 self.view.addSubview(button)
             }
         }
+        
         print(dotArray)
     }
     
     @IBAction func dotPressed(_ sender: UIButton!) {
         print("pressed a dot")
+    }
+    @IBAction func linePressed(_ sender: UIButton!) {
+        print("pressed a line")
     }
 }
 
