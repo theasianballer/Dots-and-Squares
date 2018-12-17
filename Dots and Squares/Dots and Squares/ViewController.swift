d83530adbf9be402f2489eac25005753d6728f9e
Sampreeth Krovvidi <220362@s020rmc148-05.local>
2018-12-17 11:45:21 -0600

    increment score

--- Dots and Squares/Dots and Squares/ViewController.swift
+++ Dots and Squares/Dots and Squares/ViewController.swift
@@ -12,11 +12,32 @@ class ViewController: UIViewController {
 
     // The main thing
     
+    var playerOneScore = 0
+    var playerTwoScore = 0
+    
+    func increment(for player: Int) {
+        if (player == 1) {
+            playerOneScore += 1
+        } else if (player == 2) {
+            playerTwoScore += 1
+        }
+    }
+    
+    func reset() {
+        playerOneScore = 0
+        playerTwoScore = 0
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        reset()
     }
-
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Dots and Squares/Dots and Squares/score.swift
@@ -0,0 +1,22 @@
+//
+//  score.swift
+//  Dots and Squares
+//
+//  Created by Krovvidi, Sampreeth on 12/17/18.
+//  Copyright © 2018 Currie, Chance K. All rights reserved.
+//
+
+import Foundation
+
+struct score {
+    
+    
+    
+    
+
+
+
+
+
+}
+
