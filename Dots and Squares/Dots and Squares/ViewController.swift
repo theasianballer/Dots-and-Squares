d966ca239d3c3963cd32e54e42912bc573384f4a
Bellefeuille, Neo M <157000@s020rmc148-01.local>
2018-12-17 11:43:13 -0600

    Square, Line and Dot class and Arrays

--- Dots and Squares/Dots and Squares/ViewController.swift
+++ Dots and Squares/Dots and Squares/ViewController.swift
@@ -34,12 +34,10 @@ class ViewController: UIViewController {
         for i in 0...(2*y) {
             lineArray.append([Line]())
             for j in 0...(x - (i+1)%2) {
-                array[i].append(Line(xIndex: j, yIndex: i))
-            }
-        }
-        for row in array {
-            for line in row {
-                let button = UIButton(frame: CGRect(x: line.xPos+292, y: line.yPos+406, width: line.orientation == .HORIZONTAL ? 50 : 10, height: line.orientation == .VERTICAL ? 50 : 10))
+                lineArray[i].append(Line(xIndex: j, yIndex: i))
+                var line = lineArray[i][j]
+                
+                let button = UIButton(frame: CGRect(x: line.xPos+292, y: line.yPos+406, width: line.width, height: line.height))
                 button.backgroundColor = UIColor.lightGray
                 button.setTitle("", for: [])
                 button.addTarget(self, action: #selector(linePressed), for: .touchUpInside)
@@ -57,10 +55,8 @@ class ViewController: UIViewController {
             dotArray.append([Dot]())
             for j in 0...(x) {
                 dotArray[i].append(Dot(j * 50, i * 50))
-            }
-        }
-        for row in dotArray {
-            for dot in row {
+                let dot = dotArray[i][j]
+                
                 let button = UIButton(frame: CGRect(x: dot.xpos+292, y: dot.ypos+406, width: 10, height: 10))
                 button.backgroundColor = UIColor.black
                 button.setTitle("", for: [])
@@ -80,7 +76,7 @@ class ViewController: UIViewController {
                 squareArray[i].append(Square(xIndex: j * 50, yIndex: i * 50))
                 var square = squareArray[i][j]
                 
-                let button = UIButton(frame: CGRect(x: square.xPos+125, y: square.yPos+125, width: 50, height: 50))
+                let button = UIButton(frame: CGRect(x: square.xPos+292, y: square.yPos+406, width: 50, height: 50))
                 button.backgroundColor = square.color
                 button.setTitle("", for: [])
                 button.addTarget(self, action: #selector(dotPressed), for: .touchUpInside)
@@ -96,6 +92,9 @@ class ViewController: UIViewController {
     }
     @IBAction func linePressed(_ sender: UIButton!) {
         print("pressed a line")
+    }
+    @IBAction func squarePressed(_ sender: UIButton!) {
+        print("pressed a square")
     }
 }
 
